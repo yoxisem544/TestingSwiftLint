baaf9559184b3f1a54c2f81211418304bcb9e418
David Lin <david.lin@kkday.com>
2019-10-03 16:02:46 +0800

    Initial Commit

---
+++ TestingSwiftLint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingSwiftLint
+//
+//  Created by David on 2019/10/3.
+//  Copyright © 2019 kkday. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
