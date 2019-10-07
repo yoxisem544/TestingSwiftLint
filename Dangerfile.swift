ef6e51b34efe6e0a65a1afd1b3d6c48f0346e2de
David Lin <david.lin@kkday.com>
2019-10-07 11:45:39 +0800

    ya

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,5 +1,16 @@
-import Danger 
+import Danger
+import Foundation
+
 let danger = Danger()
 
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-message("These files have changed: \(editedFiles.joined())")
+message("These files have changed: \n\(editedFiles.joined(separator: ",\n"))")
+
+let body = danger.github.pullRequest.body ?? ""
+message(body)
+
+if !body.contains("http://jira-lab.sit.kkday.com/browse/MEMAPP-") {
+    fail("You should remember to add jira link!")
+}
+
+warn("Just a test warning.")
